7f88f05bdcbcef2dcdcfca2869700f9276f2c3ea
Alexander Tiderko <alexander.tiderko@fkie.fraunhofer.de>
2020-12-16 10:53:50 +0100

    fix travis build for ubuntu 20.04

--- fkie_iop_wireshark_plugin/setup.py
+++ fkie_iop_wireshark_plugin/setup.py
@@ -24,7 +24,7 @@ try:
                   pyxbgen_exec = 'pyxbgen-py'
                result = os.system('%s -u %s --schema-root=%s --binding-root=src --module-prefix=%s -m jsidl' % (pyxbgen_exec, xsd_file, schema_dir, '%s.%s' % (package_name, 'jsidl_pyxb')))
                if result != 0:
-                  raise SystemError('error while execute pybgen\n')
+                  raise SystemError('error while execute pyxbgen\n')
          # run base class code
          super(BuildPyCommand, self).run()
 
